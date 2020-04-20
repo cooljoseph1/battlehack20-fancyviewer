6b305db38e48be4dd4bbd6c7f3ffbc9e6f156de2
cooljoseph1 <camacho.joseph@gmail.com>
2020-04-20 16:33:40 -0600

    Made the viewer look nicer + you can pause games and use arrow keys

--- fancyviewer/viewer.py
+++ fancyviewer/viewer.py
@@ -14,57 +14,41 @@ class FancyViewer:
     def __init__(self, board_size, board_states, window_size=800):        
         self.board_size = board_size
         self.board_states = board_states
-        self.window = Window(board_size, square_size=window_size // self.board_size)        
-
+        self.paused = False
+        self.window = Window(board_size, square_size=window_size // self.board_size)
+        self.window.master.bind("<Right>", self.step)
+        self.window.master.bind("<Left>", self.step_back)
+        self.window.master.bind("p", self.pause)
+        self.index = 0
+        
     def play(self, delay=0.5):
         run_time = 0
         last_time = datetime.datetime.now().timestamp()
-        for state_index in range(len(self.board_states)):
+        while True:
             while run_time < delay:
                 now = datetime.datetime.now().timestamp()
                 run_time += now - last_time
                 last_time = now
                 self.window.master.update() # Handle any events that occur
                 time.sleep(0.01)
-            
             run_time %= delay
+            if not self.paused:
+                self.step()
 
+    def pause(self, _=None):
+        self.paused ^= True
+
+    def step(self, _=None):
+        if self.index < len(self.board_states) - 1:
+            self.index += 1
             self.clear()
-            self.view(state_index)
-        
-        while True:
-            try:
-                self.window.master.update()
-            except:
-                break
-            time.sleep(0.01)
+            self.view(self.index)
 
-    def play_synchronized(self, poison_pill, delay=0.5):
-        print('')
-        
-        state_index = 0
-        run_time = 0
-        last_time = datetime.datetime.now().timestamp()
-        while state_index < len(self.board_states) or not poison_pill.is_set():
-            while len(self.board_states) <= state_index or runtime < delay:
-                now = datetime.datetime.now().timestamp()
-                run_time += now - last_time
-                last_time = now
-                self.window.master.update() # Handle any events that occur
-                time.sleep(0.01)
-
-            run_time %= delay
-
+    def step_back(self, _=None):
+        if self.index > 0:
+            self.index -= 1
             self.clear()
-            self.view(state_index)
-            state_index += 1
-
-        while True:
-            try:
-                self.window.master.update()
-            except:
-                break
-            time.sleep(0.01)
+            self.view(self.index)
           
     def clear(self):
         self.window.clear()
@@ -79,6 +63,7 @@ class Window:
         self.size = self.board_size * self.square_size
         
         self.master = tkinter.Tk()
+        self.master.title("Battlehack 2020 -- Fancy Viewer")
 
         self.canvas = tkinter.Canvas(self.master, width=self.size, height=self.size)
         self.squares = [
@@ -101,9 +86,13 @@ class Window:
         self.white_pawn = self.white_pawn.resize((self.square_size, self.square_size), Image.ANTIALIAS)
         self.white_pawn = ImageTk.PhotoImage(self.white_pawn)
 
-        self.black_pawn = Image.open(os.path.join(script_dir, "./images/black_pawn.png"))
+        self.black_pawn = Image.open(os.path.join(script_dir, "images/black_pawn.png"))
         self.black_pawn = self.black_pawn.resize((self.square_size, self.square_size), Image.ANTIALIAS)
         self.black_pawn = ImageTk.PhotoImage(self.black_pawn)
+
+        self.icon = Image.open(os.path.join(script_dir, "images/icon.png"))
+        self.icon = ImageTk.PhotoImage(self.icon)
+        self.master.iconphoto(True, self.icon)
 
 
     def clear(self):
